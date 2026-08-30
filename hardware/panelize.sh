7f50ca981c2f75dda8f045dde5e1172dc9743feb
Michael Smith <mikejsmitty@gmail.com>
2026-08-29 21:41:26 -0400

    chore: update panelize script

--- hardware/panelize.sh
+++ hardware/panelize.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-MODULE=${1%/}
-OUTPUT=$2
-PANELIZE=$3
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+MODULE=${1%/}
 MODULE_DIR="${SCRIPT_DIR}/${MODULE}"
-PCB="${MODULE_DIR}/${MODULE}.kicad_pcb" 
+OUTPUT="${MODULE_DIR}/production"
 
 if [ -z "$MODULE" ]; then
-    echo "Usage: $0 <module> [output-dir]"
+    echo "Usage: $0 <module> [panel-option|single]"
     exit 1
 fi
 
-if [ -z "$OUTPUT" ]; then
-    OUTPUT=$HOME/pdusb-panelized
+if [ -n "$2" ] && [ "$2" != "single" ]; then
+    BATCH_MOD="-p ${MODULE_DIR}/panelize-${2}.json"
 fi
 
-if [ -z "$PANELIZE" ]; then
-    kikit panelize -p "${MODULE_DIR}/panelize.json" -p :jlcTooling "${MODULE_DIR}/${MODULE}.kicad_pcb" "$OUTPUT/panel.kicad_pcb"
-    PCB="$OUTPUT/panel.kicad_pcb"
+mkdir -p "$OUTPUT"
+if [ "$2" != "single" ] && [ -e "${MODULE_DIR}/panelize.json" ]; then
+    kikit panelize -p "${MODULE_DIR}/panelize.json" $BATCH_MOD -p :jlcTooling "${MODULE_DIR}/${MODULE}.kicad_pcb" "${OUTPUT}/${MODULE}.kicad_pcb"
+    kikit fab jlcpcb --no-drc --assembly --autoname --schematic "${MODULE_DIR}/${MODULE}.kicad_sch" "${OUTPUT}/${MODULE}.kicad_pcb" "$OUTPUT/"
+else
+    kikit fab jlcpcb --no-drc --assembly --autoname --schematic "${MODULE_DIR}/${MODULE}.kicad_sch" "${MODULE_DIR}/${MODULE}.kicad_pcb" "$OUTPUT/"
 fi
-kikit fab jlcpcb --no-drc --assembly --schematic "${MODULE_DIR}/${MODULE}.kicad_sch" "$PCB" "$OUTPUT/"
